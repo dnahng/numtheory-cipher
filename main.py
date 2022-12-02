e61001b519606bf03d723e690470d3207f3e90a6
dnahng <201911863@feualabang.edu.ph>
2022-12-03 01:59:19 +0800

    Fully working na

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import os
+import sys
+
+
+# ENCRPYTION AND DECRYPTION FUNCTIONS
 def encryption(text, key):
     result = ''
     for i in range(len(text)):
@@ -23,51 +28,58 @@ def decryption(text, key):
     return result.upper()
 
 
-# check the above function
+# MENU FUNCTIONS
 def mainMenu():
-    print()
-    print(""""=== Caesar Cipher Program by Group 2 AN41 ===
-        1 - Encryption
-        2 - Decryption
-        3 - Brute Force (No key needed)
-        4 - Exit Program""")
+    try:
+        if sys.platform.startswith('win32'):
+            os.system('cls')
+        else:
+            os.system('clear')
 
-    choice = int(input("What would you like to do? "))
-    if choice == 1:
-        encMenu()
-    elif choice == 2:
-        decMenu()
-    elif choice == 3:
-        'BLANK MUNA'
-    elif choice == 4:
-        quit()
-    else:
-        "Invalid Choice, try again"
+        print("""=== Caesar Cipher Program by Group 2 AN41 ===
+            1 - Encryption
+            2 - Decryption
+            3 - Brute Force (No key needed)
+            4 - Exit Program""")
+
+        choice = int(input("What would you like to do?: "))
+        if choice == 1:
+            encMenu()
+        elif choice == 2:
+            decMenu()
+        elif choice == 3:
+            'BLANK MUNA'
+        elif choice == 4:
+            quit()
+        else:
+            print("Invalid Choice...")
+    except:
+        print('Invalid Input...')
 
 def encMenu():
-    txt = input("Please enter the message you want to encrpyt: ")
-    s = int(input("Please enter shift key (0-25): "))
-    print("Plain Text : " + txt)
-    print("Shift Key: " + str(s))
-    ans = encryption(txt, s);
-    print("Ciphertext: " + ans)
+    print("""\n=== Caesar Cipher: ENCRYPTION ===\n""")
+    try:
+        txt = input("Please enter the message you want to encrpyt: ")
+        s = int(input("Please enter shift key: "))
+        print("Plain Text : " + txt)
+        print("Shift Key: " + str(s))
+        ans = encryption(txt, s);
+        print("Ciphertext: " + ans)
+    except:
+        print("Invalid Input, try again...")
 
 def decMenu():
-    txt = input("Please enter the message you want to decrypt: ")
-    s = int(input("Please enter shift key (0-25): "))
-    print("Ciphertext : " + txt)
-    print("Shift Key: " + str(s))
-    ans = encryption(txt, s);
-    print("Plain Text: " + ans)
+    print("""\n=== Caesar Cipher: DECRYPTION ===\n""")
+    try:
+        txt = input("Please enter the message you want to decrypt: ")
+        s = int(input("Please enter shift key: "))
+        print("Ciphertext : " + txt)
+        print("Shift Key: " + str(s))
+        ans = decryption(txt, s);
+        print("Plain Text: " + ans)
+    except:
+        print("Invalid Input, try again...")
 
-# txt = "CEASER CIPHER EXAMPLE"
-# s = 4
-#
-# print("Plain txt : " + txt)
-# print("Shift Key: " + str(s))
-# ans = encryption(txt, s);
-# print("Cipher: " + ans)
-# print("Decrypt: " + decryption(ans, s))
 
 mainMenu()
 try_again = input("\nTry again? y/n: ").lower()
@@ -75,3 +87,4 @@ try_again = input("\nTry again? y/n: ").lower()
 while try_again == 'y':
     mainMenu()
     try_again = input("\nTry again? y/n: ").lower()
+
